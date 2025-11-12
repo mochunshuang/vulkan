2a56e4da1e19055f90459f3b5bd0ae7bc64d0b92
mochunshuang <1651798639@qq.com>
2025-11-13 02:56:53 +0800

    glm.cmake

--- cmake/glm.cmake
+++ cmake/glm.cmake
@@ -1,6 +1,12 @@
 # NOTE: -b 是分支。以下命令是不允许的
 # git submodule add -b 1.0.2 https://gh-proxy.com/https://github.com/g-truc/glm.git third_party/glm-1.0.2
+# NOTE: 只能先下载，然后cheout 到指定的 tag
 # git submodule add https://gh-proxy.com/https://github.com/g-truc/glm.git third_party/glm-1.0.2
+# cd third_party/glm-1.0.2
+# git checkout tags/1.0.2
+# cd ../..
+# git add .
+# git commit -m "Add glm submodule at tag 1.0.2"
 set(GLM_SOURCE_DIR "${CMAKE_SOURCE_DIR}/dependency/glm-1.0.2")
 set(GLM_BUILD_DIR "${CMAKE_SOURCE_DIR}/depend_build/glm-1.0.2")
 
