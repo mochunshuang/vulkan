e57e40ebbd1f95ff8af341146cf27998d696d2bf
mochunshuang <1651798639@qq.com>
2025-11-16 02:50:05 +0800

    ktx

--- third_party/ktx.cmake
+++ third_party/ktx.cmake
@@ -1,4 +1,10 @@
 # git submodule add https://gh-proxy.org/https://github.com/KhronosGroup/KTX-Software.git third_party/ktx
+# cd third_party/ktx
+# git checkout tags/v4.4.2
+# cd ../..
+# git add .
+# git commit -m "Add glm submodule at tag v4.4.2"
+
 add_library(ktx INTERFACE)
 target_include_directories(stb SYSTEM INTERFACE "${CMAKE_SOURCE_DIR}/third_party/ktx")
 
